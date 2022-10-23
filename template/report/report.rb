51d8c039cd64e39f12862c92281fbad4ce08ec35
Hassan Gul <wedesignerdxb@gmail.com>
2022-10-23 20:32:53 +0500

    refactor add hook methods to report class and simplifiy plain text report class

--- template/report/report.rb
+++ template/report/report.rb
@@ -9,41 +9,29 @@ class Report
     output_start
     output_head
     output_body_start
-    output_body
+    @text.each do |line|
+      output_line(line)
+    end
     output_body_end
     output_end
     
     @report_output
   end
+
+  def output_start; end
   
-  def output_body
-    @text.each do |line|
-      output_line(line)
-    end
+  def output_body_start; end
+  
+  def output_body_end; end
+  
+  def output_end; end
+  
+  def output_head
+   output_line(@title)
   end
   
   def output_line(line)
     raise 'Called abstract method: output_line'
-  end
-  
-  def output_start
-    raise 'Called abstract method: output_start'
-  end
-  
-  def output_head
-    raise 'Called abstract method: output_head'
-  end
-  
-  def output_body_start
-    raise 'Called abstract method: output_body_start'
-  end
-  
-  def output_body_end
-    raise 'Called abstract method: output_body_end'
-  end
-  
-  def output_end
-    raise 'Called abstract method: output_end'
   end
   
 end
