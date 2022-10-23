65499ff46a6310fd5c120f21a0dd1c9f4afd3c6d
Hassan Gul <wedesignerdxb@gmail.com>
2022-10-23 18:27:55 +0500

    plain report tests added

--- template/tc_report.rb
+++ template/tc_report.rb
@@ -7,4 +7,10 @@ class TestReport < Test::Unit::TestCase
     expected_html_output = "\n      <html>\n       <head>\n        <title>Monthly Report</title>\n        </head>\n      <body>\n      \n    <p>Things are going</p>\n    <p>really, really well.</p>\n        </body>\n      </html>\n      "
     assert_equal(report.output(:html), expected_html_output)
   end
+  
+  def test_plain_output
+    report = Report.new
+    expected_plain_output = "*** Monthly Report ***\n" + "Things are going\n" + "really, really well."
+    assert_equal(report.output(:plain), expected_plain_output)
+  end
 end
