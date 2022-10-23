8306c04a89058f313f4e42bf26eee81e393348ef
Hassan Gul <wedesignerdxb@gmail.com>
2022-10-23 20:12:59 +0500

    refactor test cases styling

--- template/report/tc_report.rb
+++ template/report/tc_report.rb
@@ -5,13 +5,15 @@ require 'test/unit'
 class TestReport < Test::Unit::TestCase
   def test_html_output
     report = HTMLReport.new
-    expected_html_output = "<html><head><title>Monthly Report</title></head><body><p>Things are going</p><p>really, really well.</p></body</html>"
+    expected_html_output = 
+      "<html><head><title>Monthly Report</title></head><body><p>Things are going</p><p>really, really well.</p></body</html>"
     assert_equal(report.output, expected_html_output)
   end
   
   def test_plain_output
     report = PlainTextReport.new
-    expected_plain_output = "*** Monthly Report ***\nThings are going\nreally, really well."
+    expected_plain_output =
+       "*** Monthly Report ***\nThings are going\nreally, really well."
     assert_equal(report.output, expected_plain_output)
   end
 end
