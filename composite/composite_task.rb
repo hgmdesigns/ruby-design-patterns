b7fda3e0e458afc51710147ec2be38209f229622
Hassan Gul <wedesignerdxb@gmail.com>
2022-10-28 10:44:49 +0500

    add parent refe to components

--- composite/composite_task.rb
+++ composite/composite_task.rb
@@ -7,6 +7,7 @@ class CompositeTask < Task
   
   def add_sub_task(task)
     @sub_tasks << task
+    task.parent = self
   end
   
   def <<(task)
@@ -23,6 +24,7 @@ class CompositeTask < Task
   
   def remove_sub_task(task)
     @sub_tasks.delete(task)
+    task.parent = nil
   end
   
   def get_time_required
