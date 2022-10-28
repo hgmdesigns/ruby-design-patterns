ce437c07c530b11dbe4a75fc826e1012af8da067
Hassan Gul <wedesignerdxb@gmail.com>
2022-10-28 09:47:46 +0500

    refactor composite component make batter

--- composite/make_batter_task.rb
+++ composite/make_batter_task.rb
@@ -2,27 +2,14 @@ require_relative 'task'
 require_relative 'leafs/add_dry_ingredients_task'
 require_relative 'leafs/add_liquids_task'
 require_relative 'leafs/mix_task'
+require_relative 'composite_task'
 
-class MakeBatterTask < Task
+class MakeBatterTask < CompositeTask
   def initialize
     super("Make Batter Task")
-    @sub_tasks = []
     add_sub_task(AddDryIngredientsTask.new)
     add_sub_task(AddLiquidsTask.new)
     add_sub_task(MixTask.new)
   end
-  
-  def add_sub_task(task)
-    @sub_tasks << task
-  end
-  
-  def remove_sub_task(task)
-    @sub_tasks.delete(task)
-  end
-  
-  def get_time_required
-    time = 0.0
-    @sub_tasks.each {|task| time += task.get_time_required }
-    time
-  end
+
 end
