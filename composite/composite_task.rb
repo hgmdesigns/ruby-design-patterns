2a9f026fda62c9a686e6c56327284fc99cf87a0c
Hassan Gul <wedesignerdxb@gmail.com>
2022-10-28 10:42:55 +0500

    array like methods for composite_task class implemented'

--- composite/composite_task.rb
+++ composite/composite_task.rb
@@ -9,6 +9,18 @@ class CompositeTask < Task
     @sub_tasks << task
   end
   
+  def <<(task)
+    @sub_tasks << task
+  end
+  
+  def [](index)
+    @sub_tasks[index]
+  end
+  
+  def [](index, new_value)
+    @sub_tasks[index] = new_value
+  end
+  
   def remove_sub_task(task)
     @sub_tasks.delete(task)
   end
